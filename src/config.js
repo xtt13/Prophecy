53a0f4cf3c1589c967a428e54a3c708ec43609b4
Michael Dorn <Michi@spina.vienna.sae>
2017-12-06 15:05:24 +0100

    First Commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// Game Configuration
+export default {
+
+  phaserConfig: {
+	width: window.innerWidth,
+	height: window.innerHeight,
+	renderer: Phaser.AUTO,
+	antialias: true,
+	multiTexture: false,
+	enableDebug: true
+  },
+
+  localStorageName: 'prophecy'
+};
---
+++ src/states/Boot.js
@@ -0,0 +1,38 @@
+import Phaser from 'phaser';
+import WebFont from 'webfontloader';
+
+export default class extends Phaser.State {
+  init () {
+    this.stage.backgroundColor = '#EDEEC9';
+    this.fontsReady = false;
+    this.fontsLoaded = this.fontsLoaded.bind(this);
+  }
+
+  preload () {
+
+    // Load Fonts
+    WebFont.load({
+      google: {
+        families: ['Bangers']
+      },
+      active: this.fontsLoaded
+    });
+
+    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' });
+    text.anchor.setTo(0.5, 0.5);
+
+    // Loading
+    this.load.image('loaderBg', './assets/sprites/loader-bg.png');
+    this.load.image('loaderBar', './assets/sprites/loader-bar.png');
+  }
+
+  render () {
+    if (this.fontsReady) {
+      this.state.start('Splash');
+    }
+  }
+
+  fontsLoaded () {
+    this.fontsReady = true;
+  }
+}
