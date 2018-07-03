50745ca6c6ba534fc2576f16894af92838a37756
Michael Dorn <12156832+xtt13@users.noreply.github.com>
2018-07-03 13:47:00 +0200

    Quest Dialogues Import + Format

--- src/dialoguesVillager.js
+++ src/dialoguesVillager.js
@@ -68,18 +68,117 @@ export default {
 		// Priest
 		10: [
 			'Praise the Oracle!',
-            'The Oracle and it\'s followers are a very wise \nand incredible religion, following generations \nof worshipping each and every hero that gets \nchosen by our Oracle.',
-            'We still got our hopes up, praying for the next \nhero to save us from the plant monsters hiding \nin the woods...'
+			'The Oracle and it\'s followers are a very wise \nand incredible religion, following generations \nof worshipping each and every hero that gets \nchosen by our Oracle.',
+			'We still got our hopes up, praying for the next \nhero to save us from the plant monsters hiding \nin the woods...'
 		],
 
 		// ==================
 		// Specific Dialogues Villager
 		// ==================
 
-		11: [],
-		12: [],
-		13: []
+		// Priest Quest 
+		11: ['Please, save us from this plague! The whole town\n needs you, young hero!'],
+		12: [
+			'Thank you for saving us! May you be blessed by \nthe Oracle!',
+			'What? You don’t know how you ended up here?\n Interesting.',
+			'Maybe if you stay for a while your memory will \nreturn. Feel free to stay as long as you need to.'
+		],
 
+		// Molly
+		13: [
+			'I can not believe this! Some filthy thief got her\n or his hands on my fishing rods! \nI can’t catch fish without it. Tsk!', 
+			'Hey! You! Have you seen my fishing rod? I will \ngive you a great reward if you find the thief and return my rod. \nIt would mean the world to me.'
+		],
+		14: [
+			'There it is! I hope this thief got what he deserved.', 
+			'... Just an old man? Huh. Well, at least I got my rod back. Thanks.', 
+			'Here, have one of my quality fish, my promised reward. The freshest fish you can find around Aalis.'
+		],
+
+		// Fisher
+		15: [
+			'Ugh... The fish just won’t bite lately...', 
+			'...What do you mean, not my fishing rod?', 
+			'I may or may not have found it around the forest. \nBut if you want it, you need to get me a different tool \nto catch and kill fish. \nLike, a spear maybe. That could work...'
+		],
+		16: [
+			'Oh, this is excellent! Thank you so much, young man!', 
+			'Now all I crave would be a tasty fish… Let’s hope I can get one soon.'
+		],
+		17: ['Oh my god, praise the Oracle, my deepest desires are fulfilled! You truly are a hero. Let me give you something in exchange for your efforts.'],
+		18: ['I found it a long time ago but I never found any lock it fitted. You will probably have a better use for it than me.'],
+
+		// Schmied
+		19: [''],
+		20: ['There you go. Thank you for getting the materials needed. Pleasure doing business with you.'],
+
+		// Viden
+		21: ['Would you mind helping me out a little? I need you to cut at least ten flowers and collect the petals for me.', 'It will help my research a lot.'],
+		22: ['Thank you so much, young man. I appreciate your help.'],
+
+		// Bromium
+		23: [
+			'Young man, you have proven that you mean no harm, even more, that you go out of your way to actively help the townsfolk.',
+			'Someone like you deserves a proper room to stay. Feel free to pass by the town hall to rest and take a break.'
+		],
+
+		// Ally
+		24: [
+			'Dad said I’m not allowed to leave the town but my dollis gone!',
+			'I want my doll back. It must be somewhere… Maybe on the way to the temple? Or in the woods? I can not remember...',
+			'Can you help me, Sir? Please. Pleaaseee!'
+		],
+		25: ['Thank you so much!! I will tell Dad and everyone else how awesome you are!'],
+
+		// Viden
+		26: [
+			'We need help! Quick! The plant monsters, they cornered Reya!',
+			'Please, young hero, we need your help again!'
+		],
+
+		// Reya
+		27: ['AHHHHHHHHH!!'],
+		28: [
+			'Thank you so, so much! Your name is [PLAYER NAME]? May you be blessed by the Oracle.',
+			'Let’s go back, Dad is probably worried sick.'
+		],
+
+		// Coppus
+		29: [
+			'Thank you for saving my daughter. I can not thank you enough. Just thinking what could’ve happened...',
+			'You were in the right place at the right time.',
+			'Take this. May it be a great help on the adventures and dangers you are about to face, brave hero.'
+		],
+
+		// Priest
+		30: [
+			'I haven’t seen someone as brave as you in a while. You have the true potential to be a hero... Maybe even a chosen one by the ancient prophecy.',
+			'I will let Wren know, she will inform you about the rules every hero has to follow when stepping in front of the oracle.'
+		],
+
+		// Wren
+		31: [
+			'Welcome in the temple, the Oracle’s sanctuary.',
+			'No use of weapons. No talking while the Almighty is speaking. It is very important that you show respect towards our goddess. Understood?',
+			'If you are ready, you now may step in front of the Oracle.'
+		],
+
+		// Oracle
+		32: [
+			'I can feel it… Your soul… You are a hero, aren’t you? Such an amazing, strong soul...',
+			'You have to be one of the heroes, chosen by an ancient prophecy... And as such, I have a request. To properly start your journey... To become stronger and find your true purpose.',
+			'This request is...',
+			'Please, save my town. Monsters made of vines and leaves tried to attack and take over my town.',
+			'My precious believers need you, Hero [PLAYER NAME]… Defeat the leader of these monstrosities. They hide in the woods, deep in the darkness, hidden, unseen…',
+			'It is your destiny to save us...'
+		],
+
+		// Lucy
+		33: [
+			'You got to be careful… This monster is different than anything you have faced before. Be on your guard.',
+			'Hide if necessary. You’d rather be a coward than dead, right?',
+			'I think that’s it… Good luck, [PLAYER NAME]'
+		]
 
 
 	}
