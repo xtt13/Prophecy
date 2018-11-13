bf53c7bdf5ee3b43853c320bcdb28897cf0df30b
Michael Dorn <Michael.dorn2@gmail.com>
2018-11-13 16:03:56 +0100

    Library First Floor

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -644,6 +644,7 @@ export default class extends Phaser.State {
 		this.load.tilemap('map16', 'assets/maps/map16.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.tilemap('map17', 'assets/maps/map17.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.tilemap('map18', 'assets/maps/map18.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('map19', 'assets/maps/map19.json', null, Phaser.Tilemap.TILED_JSON);
 
 
 		// Load Music
